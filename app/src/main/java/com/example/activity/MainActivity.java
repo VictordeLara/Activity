36677991df17c1ac5263ca0422609d14a0d83aad
victordelara <victorlara2010@gmail.com>
2022-05-08 16:13:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/activity/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.Serializable;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
+
+                Usuario usuario = new Usuario("Victor de lara", "victorlara2010@hotmail.com");
+
+                //passar dados
+                intent.putExtra("nome","Victor de Lara");
+                intent.putExtra("idade",22);
+                intent.putExtra("objeto", (Serializable) usuario);
+
+                startActivity(intent);
+            }
+        });
+
+        Toast.makeText(this, "onCrate", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show();
+    }
+}
